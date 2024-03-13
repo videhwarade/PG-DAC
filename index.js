a20a108baa07f81004e878e2aa388880786bc908
Videh Warade <videhwarade.cdac@gmail.com>
2024-03-13 22:40:04 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -4,3 +4,27 @@ const age = 23;
 function isabove18(age) {
   return age >= 18;
 }
+
+// create file indexedDB.js
+// $ git config--global user.email$
+// $ git config --global user.name
+// $ git init
+// $ git status
+// $ git add index.js
+// $ git status
+// $ git commit -m "initial commit"
+// $ git status
+// $ git add index.js
+// git commit -m "add age"
+// git status
+// create script.js
+// git add .
+// git commit -m "add script file"
+// git log for quite press"Q"
+// git log --oneline
+// git reset --hard bb4ff6f(ID) for revert back
+// git remote -v
+// $ git remote add origin https://github.com/videhwarade/videhwarade.git
+// $ git remote -v
+// origin  https://github.com/videhwarade/videhwarade.git (fetch)
+// origin  https://github.com/videhwarade/videhwarade.git (push)
